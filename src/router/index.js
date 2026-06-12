d6b32e1ff3f985d66c24836de5a75316000a603d
itpioneer007 <19507067964@163.com>
2026-06-12 16:27:37 +0800

    add Configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import MainHome from '../components/MainHome.vue'
-import CommunityView from '../views/CommunityView.vue'
-import AssistantView from '../views/AssistantView.vue'
-import LessonsView from '../views/LessonsView.vue'
-import ProfileView from '../views/ProfileView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainHome from "../components/MainHome.vue";
+import CommunityView from "../views/CommunityView.vue";
+import AssistantView from "../views/AssistantView.vue";
+import LessonsView from "../views/LessonsView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import CourseAnalysisReport from "../views/CourseAnalysisReport.vue";
 
-import FeaturesView from '../views/FeaturesView.vue'
-import AboutView from '../views/AboutView.vue'
+import FeaturesView from "../views/FeaturesView.vue";
+import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: MainHome },
-    { path: '/features', name: 'features', component: FeaturesView },
-    { path: '/assistant', name: 'assistant', component: AssistantView },
-    { path: '/lessons', name: 'lessons', component: LessonsView },
-    { path: '/community', name: 'community', component: CommunityView },
-    { path: '/profile', name: 'profile', component: ProfileView },
-    { path: '/about', name: 'about', component: AboutView },
+    { path: "/", name: "home", component: MainHome },
+    { path: "/features", name: "features", component: FeaturesView },
+    { path: "/assistant", name: "assistant", component: AssistantView },
+    { path: "/lessons", name: "lessons", component: LessonsView },
+    { path: "/community", name: "community", component: CommunityView },
+    { path: "/profile", name: "profile", component: ProfileView },
+    { path: "/analysis", name: "analysis", component: CourseAnalysisReport },
+    { path: "/about", name: "about", component: AboutView },
   ],
   scrollBehavior(to) {
-    if (to.hash) return { el: to.hash, behavior: 'smooth' }
-    return { top: 0 }
+    if (to.hash) return { el: to.hash, behavior: "smooth" };
+    return { top: 0 };
   },
-})
+});
 
-export default router
+export default router;
